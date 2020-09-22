6431c81adba576a5833b0b8e26679018ee924d4d
Tridib Dawn <tridibdawn1@gmail.com>
2020-09-23 00:05:51 +0530

    about us component updated

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,56 +4,52 @@
     <div class="aboutus-area">
       <div class="container">
         <div class="row">
-          <div class="col-xs-12">
-            <div class="col-md-4 col-sm-6 col-xs-12">
-              <div class="aboutus-image float-left hidden-sm">
-                <img
-                  src="https://devitems.com/preview/sopnovilla/sopnovilla/img/about/1.jpg"
-                  alt
-                />
-              </div>
+          <!-- <div class="col-xs-12"> -->
+          <div class="col-md-4 col-sm-6 col-xs-12">
+            <div class="aboutus-image float-left hidden-sm">
+              <img
+                src="https://lh5.googleusercontent.com/p/AF1QipMq5BnaefzfsIcoNcZ5qQSB-jcbXNQq8hO3S6XW=w408-h265-k-no"
+              />
             </div>
-            <div class="col-md-8 col-sm-6 col-xs-12">
-              <div class="aboutus-content">
-                <h1>
-                  aboutus
-                  <span>Sopno</span>
-                </h1>
-                <h4>Property Details</h4>
-                <p>
-                  Contrary to popular belief, Lorem Ipsum is not simply random
-                  text. It has oots in a piece of classitin literature from 45
-                  BC, making it over 2000 years old. Richard McClint professor
-                  at Hamden dney College irginia, looked up one of the more
-                  obscure Latin words, consectetur, from a Lorem Ipsum passage,
-                  and going through the cites of the word in classical
-                  literature.
-                </p>
+          </div>
+          <div class="col-md-8 col-sm-6 col-xs-12">
+            <div class="aboutus-content">
+              <h1>
+                About
+                <span>Movie Mania</span>
+              </h1>
+              <h4>Our Story</h4>
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Perspiciatis praesentium earum pariatur hic quasi velit cumque
+                quas debitis exercitationem! Libero aliquam facere totam alias
+                soluta modi fuga laborum error ea?
+              </p>
 
-                <div class="counter">
-                  <div class="single-counter text-center">
-                    <h2 class="counter">1500</h2>
-                    <p>Square Feet</p>
-                  </div>
+              <div class="counter">
+                <div class="single-counter text-center">
+                  <h2 class="counter">15000+</h2>
+                  <p>Total Movies</p>
+                </div>
 
-                  <div class="single-counter text-center">
-                    <h2 class="counter">10</h2>
-                    <p>Bedrooms</p>
-                  </div>
+                <div class="single-counter text-center">
+                  <h2 class="counter">1M+</h2>
+                  <p>Loyal Customers</p>
+                </div>
 
-                  <div class="single-counter text-center">
-                    <h2 class="counter">5</h2>
-                    <p>Bathrooms</p>
-                  </div>
+                <div class="single-counter text-center">
+                  <h2 class="counter">50+</h2>
+                  <p>International Sponsors</p>
+                </div>
 
-                  <div class="single-counter text-center">
-                    <h2 class="counter">6</h2>
-                    <p>Car Parking</p>
-                  </div>
+                <div class="single-counter text-center">
+                  <h2 class="counter">100+</h2>
+                  <p>Global Awards</p>
                 </div>
               </div>
             </div>
           </div>
+          <!-- </div> -->
         </div>
       </div>
     </div>
